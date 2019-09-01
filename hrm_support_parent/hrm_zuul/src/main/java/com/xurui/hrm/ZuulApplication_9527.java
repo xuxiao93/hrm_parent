5514ae0f9dccf0d5dfe51efcb810d64a0034682a
xuxiao93 <1216826754@qq.com>
2019-09-01 22:16:21 +0800

    提交

--- hrm_support_parent/hrm_zuul/src/main/java/com/xurui/hrm/ZuulApplication_9527.java
+++ hrm_support_parent/hrm_zuul/src/main/java/com/xurui/hrm/ZuulApplication_9527.java
@@ -1,10 +1,12 @@
-package com.xurui.zuul;
+package com.xurui.hrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApplication_9527 {
     public static void main(String[] args) {
